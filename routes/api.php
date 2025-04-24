28d383b1fcaac725a95f4cbaf954a9d6510907c7
BrianCamunez <124356373+BrianCamunez@users.noreply.github.com>
2025-04-24 17:18:29 +0000

    Routas

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\TargetaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+ })->middleware('auth:sanctum');
+
+
+Route::get('/targeta', [TargetaController::class, 'index']);
+
+Route::get('/targeta/{id}', [TargetaController::class, 'show']);
+
+Route::post('/targeta', [TargetaController::class, 'store']);
+
+Route::patch('/targeta/{id}', [TargetaController::class, 'updatePartial']);
+
+Route::put('/targeta/{id}', [TargetaController::class, 'update']);
+
+Route::delete('/targeta/{id}', [TargetaController::class, 'destroy']);
+
+
+
